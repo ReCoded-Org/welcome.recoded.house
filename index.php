06147a86fe82eb9944eada650b87f37ced7521d3
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-03-03 19:36:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ body, a {
 .div-content {
 	display: block;
 	padding: 100px 20px;
-	background: #333;
+	background: rgba(65,65,65,1);
 	column-count: 3;
 	column-gap: 20px;
 	column-rule: 0 solid rgba(200,200,200,0.5);

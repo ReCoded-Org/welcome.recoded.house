3b5465a526facdd7bf3aa7b3b30a9f9fdedc3c76
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-03-03 15:47:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
+<? $verbiage = file_get_contents('verbiage.json');
+$verbiage = json_decode($verbiag, true); ?>
 
+<!doctype html>
+<html amp lang="en">
+<head>
+<meta charset="utf-8">
+	
+<!-- AMP -->
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+<!-- More AMP -->
+<script async custom-element="amp-fx-collection" src="https://cdn.ampproject.org/v0/amp-fx-collection-0.1.js"></script>
+
+<title><? echo $verbiage['title'] ?></title>
+<link rel="canonical" href="<? echo $verbiage['domain'] ?>">
+	
+<!-- Import custom fonts -->
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Noto+Serif+SC" rel="stylesheet">	
+<link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+	
+<!-- Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- Custom CSS -->
+<style amp-custom>
+
+</style></head>
+<body>
